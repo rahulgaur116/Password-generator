cfad74e77986283e025a5fced27c610530d0a43b
Rahul Sharma <rahulgaur116@gmail.com>
2024-02-13 19:08:46 -0500

    cleared script code, renamed some variables to e more descriptive

--- Develop/script.js
+++ Develop/script.js
@@ -7,49 +7,46 @@ function generatePassword() {
   var password = "";
 
   // Window prompt commands
-  // Prompt the user for the length of the password
+ 
   var passwordLength = prompt("Enter the length of the password between 8 and 128 characters:");
 
-  // Check if the user entered a valid length
+  
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Invalid password length! Please enter a number between 8 and 128.");
     return;
   }
 
   // Window confirm Commands
-  // Prompt the user for password criteria
+  var includeNumbers = confirm("Include numbers?");
   var includeUppercase = confirm("Include uppercase letters?");
   var includeLowercase = confirm("Include lowercase letters?");
-  var includeNumbers = confirm("Include numbers?");
   var includeSpecialChars = confirm("Include special characters?");
 
-  // Check if the user selected at least one criteria
+
   if (!includeUppercase && !includeLowercase && !includeNumbers && !includeSpecialChars) {
     alert("Please select at least one criteria for the password.");
     return;
   }
 
   // Define the character sets based on the selected criteria
+  var numberCharacters = "0123456789";
   var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
-  var numberCharacters = "0123456789";
   var specialCharacters = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
 
 
   // Generate the password based on the selected criteria and length
   for (var i = 0; i < passwordLength; i++) {
-    // Select a random character set based on the selected criteria
     var charSet = "";
     if (includeUppercase) charSet += uppercaseCharacters;
     if (includeLowercase) charSet += lowercaseCharacters;
     if (includeNumbers) charSet += numberCharacters;
     if (includeSpecialChars) charSet += specialCharacters;
 
-    // Select a random character from the character set
-    var randomChar = charSet.charAt(Math.floor(Math.random() * charSet.length));
+    var randomCharacter = charSet.charAt(Math.floor(Math.random() * charSet.length));
 
     // Append the random character to the password
-    password += randomChar;
+    password += randomCharacter;
   }
 
   return password; // return generated password
